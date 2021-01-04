3797ea7f577e79ef414908d2bb4e7f3610bc8a5b
Jede Brahma <brahmajede@gmail.com>
2021-01-04 19:24:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,10 +2,12 @@ def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
+
 def toadstool
   phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts phrase
 end
+
 def link
   phrase = "It's Dangerous To Go Alone! Take This.!"
   puts phrase
