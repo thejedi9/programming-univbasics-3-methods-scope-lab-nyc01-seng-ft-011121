e3761e5a07d3e7aba9468dfd96a8a0af4bfce63a
Jede Brahma <brahmajede@gmail.com>
2021-01-04 19:02:14 +0000

    Automatically backed up by Learn

---
+++ lib/catch_phrases.rb
@@ -0,0 +1,7 @@
+describe "all_phrases" do
+  it "puts out all of the previous catch phrases" do
+    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
+  end
+end
