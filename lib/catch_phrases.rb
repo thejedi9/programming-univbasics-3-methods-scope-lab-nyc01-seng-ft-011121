1200d04a35416021c467437327b6c053812cfbb8
Jede Brahma <brahmajede@gmail.com>
2021-01-04 19:18:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,11 +2,3 @@ def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
-
-def phrases 
-  status = "puts out all of the previous catch phrases" 
-  all_phrases = "It's-a me, Mario!" 
-  all_phrases = "Thank You Mario! But Our Princess Is In Another Castle!" 
-  all_phrases = "Dangerous To Go Alone! Take This." 
-end 
-end
