87715427571077f6ba56c3d78b50bb492d0ca77c
Jede Brahma <brahmajede@gmail.com>
2021-01-04 19:20:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,7 @@ def toadstool
   phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts phrase
 end
+def link
+  phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts phrase
+end
